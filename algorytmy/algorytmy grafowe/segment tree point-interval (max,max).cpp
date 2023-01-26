f11691d18f891f83e224696e59566446f2ae4752
kubapoke <75582093+kubapoke@users.noreply.github.com>
2023-01-26 18:25:26 +0100

    Add files via upload

---
+++ algorytmy/algorytmy grafowe/segment tree point-interval (max,max).cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define NTREE 1048576 //pierwsza potêga dwójki wiêksza ni¿ iloœæ elementów
+int tree[NTREE*2];
+
+void update(int pos, int val)
+{
+	pos+=NTREE;
+	while(pos)
+	{
+		tree[pos]=max(tree[pos],val);
+		pos/=2;
+	}
+}
+
+int query(int l, int r)
+{
+	l+=NTREE; r+=NTREE;
+	int ans=0;
+	
+	if(l==r)
+	{
+		return tree[l];
+	}
+	
+	ans=max(tree[l],tree[r]);
+	
+	while(r-l>1)
+	{
+		if(l%2==0) ans=max(ans,tree[l+1]);
+		if(r%2==1) ans=max(ans,tree[r-1]);
+		l/=2;
+		r/=2;
+	}	
+	
+	return ans;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(0);	
+	return 0;
+}
